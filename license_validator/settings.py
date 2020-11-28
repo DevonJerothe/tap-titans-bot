c89fe10b7990ed88326b03ddd8d921b56a9ec6a3
Votum <theycallmevotum@gmail.com>
2020-11-28 13:10:16 -0400

    #1 Add level max options to skill level functionality.
    - Add configuration to determine amount of clicks per skill.
    - 0 will disable the levelling of a given skill.

--- license_validator/settings.py
+++ license_validator/settings.py
@@ -57,10 +57,10 @@ LOCAL_DATA_DEPENDENCIES_DIRECTORY = path.join(LOCAL_DATA_DIRECTORY, "dependencie
 LOCAL_DATA_LOGS_DIRECTORY = path.join(LOCAL_DATA_DIRECTORY, "logs")
 # We'll also store our configurations locally so that a user can stop/start
 # a program without retrieving configurations every time.
-LOCAL_DATA_CONFIGURATIONS_FILE = path.join(LOCAL_DATA_DIRECTORY, "configurations.json")
+LOCAL_DATA_CONFIGURATIONS_FILE = path.join(LOCAL_DATA_DIRECTORY, "configurations.titan")
 # The configuration file itself should be populated with a default configuration
 # (handled elsewhere), and allow for configurations to be specified by the user.
-LOCAL_DATA_CONFIGURATION_FILE = path.join(LOCAL_DATA_DIRECTORY, "configuration.json")
+LOCAL_DATA_CONFIGURATION_FILE = path.join(LOCAL_DATA_DIRECTORY, "configuration.txt")
 # The license information should be stored in our local data directory
 # in a proper text file.
 LOCAL_DATA_LICENSE_FILE = path.join(LOCAL_DATA_DIRECTORY, "license.txt")
