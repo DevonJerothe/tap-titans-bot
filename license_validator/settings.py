6a175b4ee6711bee9b5671a06d55e335fcb19fa8
Votum <theycallmevotum@gmail.com>
2020-12-13 13:06:40 -0400

    Rotate application secret (0.0.5)

--- license_validator/settings.py
+++ license_validator/settings.py
@@ -8,7 +8,7 @@ from os import path
 # and used to store our needed local data.
 VALIDATION_NAME = "tap-titans-bot"
 # Validation Config Identifier Secret.
-VALIDATION_IDENTIFIER_SECRET = "4MPMbMcXMYsb.'.ZNc%H_o"
+VALIDATION_IDENTIFIER_SECRET = "PAbb|bZc1.'9M4cHH%{"
 
 # Base Validation URL.
 # This should be the main url with a trailing slash included.
