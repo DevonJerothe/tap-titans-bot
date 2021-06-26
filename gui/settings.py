57b8c2eee8516ba7ab4ec1de2ae5d538e166fae8
Votum <theycallmevotum@gmail.com>
2021-06-26 10:11:35 -0300

    #223 Update gui to support new open sourced/localized application
    - Remove menu functionality that accessed backend api for data
    - Add support for creating/editing/deleting certain database models
    -

--- gui/settings.py
+++ gui/settings.py
@@ -2,12 +2,14 @@ from pathlib import Path
 from os import path
 
 
-# Build paths inside our project like this: BASE_DIRECTORY / "subdir".
-PROJECT_DIRECTORY = Path(__file__).resolve().parent.parent
-MEDIA_DIRECTORY = path.join(PROJECT_DIRECTORY, "media")
+MEDIA_DIRECTORY = path.join(Path(__file__).resolve().parent.parent, "media")
 # Retrieve the file that will be used as an icon for use
 # with the system tray implementation.
 ICON_FILE = path.join(MEDIA_DIRECTORY, "flame.ico")
+
+# The dt format here is used when displaying event times
+# to ensure a more condensed format is used.
+DT_FORMAT = "%m/%d/%Y %I:%M:%S"
 
 # Menu definitions can make use of these constants to aid in
 # the generation of menu entries.
@@ -24,28 +26,15 @@ MENU_START_EVENT = "Start"
 MENU_STOP_SESSION = "Stop Session"
 MENU_RESUME_SESSION = "Resume Session"
 MENU_PAUSE_SESSION = "Pause Session"
-MENU_UPDATE_LICENSE = "Update License"
+MENU_EVENTS = "Events"
 MENU_TOOLS = "Tools"
-MENU_TOOLS_CHECK_FOR_UPDATES = "Check For Updates"
 MENU_TOOLS_LOCAL_DATA = "Local Data"
 MENU_TOOLS_GENERATE_DEBUG_SCREENSHOT = "Generate Debug Screenshot"
 MENU_TOOLS_MOST_RECENT_LOG = "Most Recent Log"
-MENU_TOOLS_FLUSH_LICENSE = "Flush License"
 MENU_INSTANCES = "Instances"
-MENU_INSTANCES_EDIT_INSTANCES = "Edit Instances"
-MENU_INSTANCES_REFRESH_INSTANCES = "Refresh Instances"
+MENU_CONFIGURATIONS_ADD = "Add Configuration"
 MENU_CONFIGURATIONS = "Configurations"
-MENU_CONFIGURATIONS_EDIT_CONFIGURATIONS = "Edit Configurations"
-MENU_CONFIGURATIONS_REFRESH_CONFIGURATIONS = "Refresh Configurations"
-MENU_LOCAL_SETTINGS = "Local Settings"
-MENU_LOCAL_SETTINGS_ENABLE_TOAST_NOTIFICATIONS = "Enable Toast Notifications"
-MENU_LOCAL_SETTINGS_DISABLE_TOAST_NOTIFICATIONS = "Disable Toast Notifications"
-MENU_LOCAL_SETTINGS_ENABLE_FAILSAFE = "Enable Failsafe"
-MENU_LOCAL_SETTINGS_DISABLE_FAILSAFE = "Disable Failsafe"
-MENU_LOCAL_SETTINGS_ENABLE_AD_BLOCKING = "Enable Ad Blocking"
-MENU_LOCAL_SETTINGS_DISABLE_AD_BLOCKING = "Disable Ad Blocking"
-MENU_LOCAL_SETTINGS_ENABLE_AUTO_UPDATE = "Enable Auto Updates"
-MENU_LOCAL_SETTINGS_DISABLE_AUTO_UPDATE = "Disable Auto Updates"
+MENU_SETTINGS = "Settings"
 
 MENU_DISCORD = "Discord"
 MENU_EXIT = "Exit"
