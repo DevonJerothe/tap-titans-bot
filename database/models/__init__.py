01edbf36091102d2699f71345af7e09f4a8c77a0
Votum <theycallmevotum@gmail.com>
2021-07-05 09:32:41 -0300

    #231 Add django orm and remove instances of peewee framework
    - Add new migrations framework setup, proper migrations dir within database app
    - Update models to use proper django imports where applicable
    - Update application entry point to call migrate command on startup
    - Update settings to include required options and database setup

---
+++ database/models/__init__.py
@@ -0,0 +1,4 @@
+from database.models.configuration import Configuration
+from database.models.event import Event
+from database.models.instance import Instance
+from database.models.settings import Settings
